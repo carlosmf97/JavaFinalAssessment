d0cc938e9f76aca19cde178d832f800bdc90e68b
Munoz Carlos <carlos.munoz@solera.com>
2022-11-17 12:50:26 +0100

    Backend finished

--- backend/src/main/java/com/finalAssessment/database/users/UserController.java
+++ backend/src/main/java/com/finalAssessment/database/users/UserController.java
@@ -1,9 +1,10 @@
 package com.finalAssessment.database.users;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,14 +27,39 @@ public class UserController {
 		return repository.findAll();
 	}
 	
+	@GetMapping("/users/{id}")
+	private User getUser_byId(@PathVariable String id) {
+		Optional<User> getUser = repository.findById(Integer.parseInt(id));
+		if(getUser != null) {
+			return getUser.get();
+		}
+		return null;
+	}
+	
 	@PostMapping("/users")
 	private void addNewUser(@RequestBody User newUser) {
 		repository.save(newUser);
 	}
 	
-	@PutMapping("/users/{id}/{variable}")
-	private void setNewVariable(@PathVariable String variable) {
-		
+	@PutMapping("/users/{id}")
+	private void setNewVariable(@RequestBody User user, @PathVariable String id) {
+		Optional<User> getUser = repository.findById(Integer.parseInt(id));
+		if(getUser != null) {
+			getUser.get().setUsername(user.getUsername());
+			getUser.get().setLastname(user.getLastname());
+			getUser.get().setEmail(user.getEmail());
+			getUser.get().setPhone(user.getPhone());
+			repository.save(getUser.get());
+		}
 	}
+	
+	@DeleteMapping("/users/{id}/delete")
+	private void deleteUser(@PathVariable String id) {
+		Optional<User> getUser = repository.findById(Integer.parseInt(id));
+		if(getUser != null) {
+			repository.delete(getUser.get());
+		}
+	}
+	
 	
 }
